5ec37e86399a44e553d1dd3c904415fe16d22cf0
youna <youna0720@postech.ac.kr>
2023-11-04 12:29:26 +0900

    temporary

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -18,3 +18,113 @@ syscall_handler (struct intr_frame *f UNUSED)
   printf ("system call!\n");
   thread_exit ();
 }
+
+int 
+wait (pid_t pid)
+{
+  return -1;   
+}
+
+bool 
+create (const char *file, unsigned initial_size)
+{
+  return filesys_create(file, initial_size);
+}
+
+bool 
+remove (const char *file)
+{
+  return filesys_remove(file);
+}
+
+int 
+open (const char *file)
+{
+  struct file *opened_file = filesys_open(file);
+  struct thread *t = thread_current();
+
+  if (opened_file == NULL)
+    return -1;
+  t->file_descriptor[t->file_next_idx] = opened_file;
+  return t->file_next_idx++;
+}
+
+int 
+filesize (int fd)
+{
+  struct thread *t = thread_current();
+  struct file *idxed_file = t->file_descriptor[fd];
+  
+  if (idxed_file == NULL)
+    return -1;
+  return file_length(idxed_file);
+}
+
+int 
+read (int fd, void *buffer, unsigned length)
+{
+  struct thread *t = thread_current();
+  struct file *toread_file;
+  int i;
+
+  if (fd == 0)
+  {
+    for (i = 0; i < length; i++) 
+      *(char *)(buffer + i) = input_getc();
+    return length;
+  }
+  toread_file = t->file_descriptor[fd];
+  if (toread_file == NULL)
+    return -1;
+  return file_read(toread_file, buffer, length);
+}
+
+int 
+write (int fd, const void *buffer, unsigned length)
+{
+  struct thread *t = thread_current();
+  struct file *towrite_file;
+
+  if (fd == 1)
+  {
+    putbuf(buffer, length);
+    return length;
+  }
+  towrite_file = t->file_descriptor[fd];
+  if (towrite_file == NULL)
+    return -1;
+  return file_write(towrite_file, buffer, length);
+}
+
+void 
+seek (int fd, unsigned position)
+{
+  struct thread *t = thread_current();
+  struct file *toseek_file;
+    
+  toseek_file = t->file_descriptor[fd];
+  if (toseek_file == NULL)
+    return;
+  file_seek(toseek_file, position);
+  return;
+}
+
+unsigned 
+tell (int fd)
+{
+  struct thread *t = thread_current();
+  struct file *totell_file;
+    
+  totell_file = t->file_descriptor[fd];
+  return file_tell(totell_file);
+}
+
+void 
+close (int fd)
+{
+  struct thread *t = thread_current();
+  file_close(t->file_descriptor[fd]);
+  t->file_descriptor[fd] = NULL;
+  if (fd + 1 == t->file_next_idx)
+    t->file_next_idx--;
+}
