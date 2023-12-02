4995b4190d0c8472310a62ee8d72b9575573fd98
Jinwoo Park <qkrlwsnd@naver.com>
2023-12-02 22:59:53 +0900

    Add File Memory Mapping Struct to <page.h>

--- src/vm/page.h
+++ src/vm/page.h
@@ -12,10 +12,18 @@ struct vm_entry {
   size_t page_read_bytes;
   size_t page_zero_bytes;
   off_t ofs;
-
+  
   bool writable;
   size_t swap_idx;
   struct hash_elem elem;
+  struct list_elem mmap_elem;
+};
+
+struct mmap_file {
+  int mapid;
+  struct file *mapped_file;
+  struct list_elem elem;
+  struct list vme_list;
 };
 
 void vm_init(struct hash *);
