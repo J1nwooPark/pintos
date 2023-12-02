dc2b32beaeac2463b55b452a39013984795e9d8c
Jinwoo Park <qkrlwsnd@naver.com>
2023-12-02 22:58:37 +0900

    Syscall mmap(), unmap() Done

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -8,6 +8,7 @@
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 #include "threads/synch.h"
+#include "threads/malloc.h"
 #include "filesys/file.h"
 #include "filesys/filesys.h"
 #include "filesys/inode.h"
@@ -56,6 +57,7 @@ check_valid_string (void *str)
     str++;
   }  
 }
+
 void
 syscall_init (void) 
 {
@@ -169,6 +171,22 @@ syscall_handler (struct intr_frame *f UNUSED)
       close(*(int *)(esp + 4));
       lock_release (&file_lock);
       break;
+    case SYS_MMAP:
+      check_address(esp + 8);
+    
+      lock_acquire (&file_lock);
+      ret = mmap(*(int *)(esp + 4), *(void **)(esp + 8));
+      lock_release (&file_lock);
+          
+      f->eax = ret;
+      break;
+    case SYS_MUNMAP:
+      check_address(esp + 4);
+          
+      lock_acquire (&file_lock);
+      munmap(*(int *)(esp + 4));
+      lock_release (&file_lock);
+      break;
   }
 }
 
@@ -194,7 +212,6 @@ exec (const char *file)
   struct thread *cur = thread_current();
   tid_t child_tid = process_execute(file);
   struct list_elem *e;
-
 
   for (e = list_begin(&cur->childs); e != list_end(&cur->childs); e = list_next(e))
   {
@@ -318,4 +335,83 @@ close (int fd)
   t->file_descriptor[fd] = NULL;
   if (fd + 1 == t->file_next_idx)
     t->file_next_idx--;
+}
+
+mapid_t
+mmap (int fd, void *addr)
+{
+  struct thread* t = thread_current();
+  struct file *tomap_file = t->file_descriptor[fd], *new_file;
+  off_t len;
+  void *page_start;
+  int i, page_cnt;
+    
+  if (fd <= 1 || addr == 0 || tomap_file == NULL) 
+    return -1;
+  len = file_length(tomap_file);
+  if (len == 0)
+    return -1;
+  page_cnt = (len / PGSIZE) + ((len % PGSIZE) ? 1 : 0);
+  page_start = pg_round_down(addr);
+  if (addr != page_start)
+    return -1;
+  for (i = 0; i < page_cnt; i++)
+  {
+    struct vm_entry *vme = find_vme(page_start + i * PGSIZE);
+    if (vme != NULL)
+      return -1;
+  }
+    
+  new_file = file_reopen(tomap_file);
+  struct mmap_file *mfile = malloc (sizeof *mfile);
+  mfile->mapid = t->mmap_next_idx++;
+  mfile->mapped_file = new_file;
+  list_init(&mfile->vme_list);
+  list_push_back(&t->mmap_list, &mfile->elem);
+
+  for (i = 0; i < page_cnt; i++)
+  {
+    struct vm_entry *vme = malloc (sizeof *vme); 
+    vme->vaddr = page_start + i * PGSIZE;
+    vme->file = new_file;
+    vme->page_read_bytes = (len >= PGSIZE) ? PGSIZE : len;
+    vme->page_zero_bytes = PGSIZE - vme->page_read_bytes;
+    vme->ofs = i * PGSIZE;
+    vme->writable = true;
+    insert_vme(&t->vm, vme);
+    list_push_back(&mfile->vme_list, &vme->mmap_elem);
+    len -= PGSIZE;
+  }
+  return mfile->mapid;
+}
+
+void
+munmap (mapid_t mapid)
+{
+  struct thread *t = thread_current();
+  struct list_elem *e, *e2;
+    
+  for (e = list_begin(&t->mmap_list); e != list_end(&t->mmap_list);
+       e = list_next (e))
+  {
+    struct mmap_file *mfile = list_entry(e, struct mmap_file, elem);
+    if (mfile->mapid == mapid)
+    {
+      for (e2 = list_begin(&mfile->vme_list); e2 != list_end(&mfile->vme_list);)
+      {
+        struct vm_entry *vme = list_entry(e2, struct vm_entry, mmap_elem);
+        void *vaddr = vme->vaddr;
+        bool isDirty = pagedir_is_dirty(t->pagedir, vaddr);
+        
+        if (isDirty)
+          file_write_at(mfile->mapped_file, vaddr, vme->page_read_bytes, vme->ofs);
+        e2 = list_remove(e2);
+        delete_vme(&t->vm, vme);
+        free(vme);
+      }
+      e = list_remove(e);
+      free(mfile);
+      return;
+    }
+  }
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -6,6 +6,7 @@
 #include "vm/page.h"
 
 typedef int pid_t;
+typedef int mapid_t;
 
 struct vm_entry *check_address(void *);
 void check_valid_buffer (void *, unsigned, bool);
@@ -25,5 +26,8 @@ int write (int fd, const void *buffer, unsigned length);
 void seek (int fd, unsigned position);
 unsigned tell (int fd);
 void close (int fd);
+
+mapid_t mmap (int fd, void *addr);
+void munmap (mapid_t mapid);
 
 #endif /* userprog/syscall.h */
